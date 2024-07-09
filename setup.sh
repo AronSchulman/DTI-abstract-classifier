2550bfabe93738ffdc075f467cad1d2ae4fcbdb2
AronSchulman <aron.schulman@gmail.com>
2024-07-09 16:06:48 +0300

    Fix a bug related to package installation location

--- setup.sh
+++ setup.sh
@@ -2,14 +2,16 @@
 
 if [ -d "DTI-venv" ]; then
     echo "Virtual environment 'DTI-venv' activated."
+    source DTI-venv/bin/activate
 else
     python3 -m venv DTI-venv
+    source DTI-venv/bin/activate
     pip install -r requirements.txt
     echo "Virtual environment 'DTI-venv' created."
     echo "Setup complete. Virtual environment 'DTI-venv' is ready with required packages installed."
 fi
 
-source DTI-venv/bin/activate
+
 
 
 
